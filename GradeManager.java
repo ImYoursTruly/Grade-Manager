9501f98db3459e8e5a5203c1c46ca7e3cdb73d2d
yours truly <marcelinoraymundo455@gmail.com>
2026-02-17 14:14:44 +0800

    Data storing changes from 2D array to object oriented
    Rebuilt code structure from relying to arrays/2D arrays to store data to relying on objects

--- GradeManager.java
+++ GradeManager.java
@@ -2,83 +2,64 @@ package yourstruly.OOPPractice;
 
 public class GradeManager{
     
-    private String[] students,studentID, subjects, letterGrade, remarks;
-    private int[][] grades;
-    private int[] average;
+    private String[] subjects;
+    private String studentName, studentID, letterGrade, remarks;
+    private int[] grades;
+    private int average, count = 0;
     
-    public GradeManager(String[] std, String[] stdID, String[] sbs, int[][] grd){
-        this.students = std;
-        this.studentID = stdID;
-        this.subjects = sbs;
-        this.grades = grd;
-        average = new int[students.length];
-        letterGrade = new String[students.length];
-        remarks = new String[students.length];
+    GradeManager(String student, String studentID, String[] subjects, int[] grades){
+        this.studentName = student;
+        this.studentID = studentID;
+        this.subjects = subjects;
+        this.grades = grades;
     }
     
-    public int[] computeAverage(){
-        for(int x = 0; x < grades.length; x++){
-            int sum = 0;
-            for(int y = 0; y < grades[x].length; y++){
-                sum += grades[x][y];
-            } 
-            average[x] = sum / subjects.length;
-        }
-        return average;
+    public int computeAverage(){
+        int sumOfGrades = 0;
+        for(int i = 0 ; i < grades.length; i++)
+            sumOfGrades += grades[i];
+        return average = sumOfGrades / grades.length;
     }
+        
     
-    public String[] computeLetterGrade(){
-        for(int x = 0; x < letterGrade.length; x++){
-            if(average[x] >= 90 && average[x] <= 100)
-                letterGrade[x] = "A";
-            else if(average[x] >= 80 && average[x] <= 89)
-                letterGrade[x] = "B";
-            else if(average[x] >= 70 && average[x] <= 79)
-                letterGrade[x] = "C";
-            else if(average[x] >= 60 && average[x] <= 69)
-                letterGrade[x] = "D";
-            else if(average[x] < 60)
-                letterGrade[x] = "F";
-            else
-                System.out.print("Error");
-        }        
+    public String computeLetterGrade(){
+        computeAverage();
+        if(average >= 90 && average <= 100)
+            letterGrade = "A";
+        else if(average >= 80 && average <= 89)
+            letterGrade = "B";
+        else if(average >= 70 && average <= 79)
+            letterGrade = "C";
+        else if(average >= 60 && average <= 69)
+            letterGrade = "D";
+        else if(average < 60)
+            letterGrade = "F";
+        else
+            letterGrade = "Error";
         return letterGrade;
     }
     
-    public String[] computeRemarks(){
-        for(int i = 0; i < remarks.length; i++){
-            if(average[i] >= 60 && average[i] <= 100)
-                remarks[i] = "Passed";
-            else if(average[i] >= 0 &&average[i] < 60)
-                remarks[i] = "Failed";
-            else
-                System.out.print("Error");
-        }
+    public String computeRemarks(){
+        computeAverage();
+        if(average >= 60 && average <= 100)
+            remarks = "Passed";
+        else if(average >= 0 && average <= 59)
+            remarks = "Failed";
+        else
+            remarks = "Error";
         return remarks;
     }
     
-    public void displayStudentInfo(int[] ave, String[] letterGrd, String[] rem){
-        
-        for(int i = 0; i < students.length; i++){
-            System.out.println("Student No." + (i + 1));
-            System.out.println("Student name: " + students[i]);
-            System.out.println("Student ID: " + studentID[i]);
-            System.out.println("Student average: " + ave[i]);
-            System.out.println("Letter grade: " + letterGrd[i]);
-            System.out.println("Remarks: " + rem[i] + "\n");
-        }
+    public String getName(){
+        return studentName;
     }
     
-    public void displayAnalytics(String[] std, int[] ave){
-        String highestStudent = "";
-        int highestGrade = 0;
-        for(int i = 0; i < ave.length; i++){
-            if(ave[i] > highestGrade){
-                highestGrade = ave[i];
-                highestStudent = std[i];
-            }
-        }
-        
-        System.out.println("Highest average: " + highestStudent + " -> " + highestGrade);
-    }
+    public void displayStudentProfile(){
+        computeAverage(); computeLetterGrade(); computeRemarks();
+        System.out.println("Name: " + studentName);
+        System.out.println("ID: " + studentID);
+        System.out.println("Average: " + average);
+        System.out.println("Grade: " + letterGrade);
+        System.out.println("Remarks: " + remarks);
+    }    
 }
